c40f82b63988aa9f844d559909ba690c44843aab
Cat MarquezMontas <catherine.marquezmontas@tlgcohort.com>
2023-02-21 14:50:51 -0800

    Lab 9.1 complete

---
+++ Lab09.1/src/com/hr/personnel/HourlyEmployee.java
@@ -0,0 +1,49 @@
+package com.hr.personnel;
+
+import gov.irs.TaxPayer;
+
+import java.time.LocalDate;
+
+public class HourlyEmployee extends Employee {
+    private double rate;
+    private double hours;
+
+    public HourlyEmployee() {
+    }
+
+    public HourlyEmployee(String name, LocalDate hireDate, double rate, double hours) {
+        super(name, hireDate);
+        setRate(rate);
+        setHours(hours);
+    }
+
+    @Override
+    public void pay() {
+        System.out.println(getName() + " Employee is paid hourly " + getRate() * getHours());
+    }
+
+    public void payTaxes() {
+        System.out.println(getName() + " paid taxes of " + getRate() * getHours() * HOURLY_TAX_RATE);
+    }
+
+    public double getRate() {
+        return this.rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public double getHours() {
+        return this.hours;
+    }
+
+    public void setHours(double hours) {
+        this.hours = hours;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", rate=" + getRate() + ", hours=" + getHours();
+    }
+}
