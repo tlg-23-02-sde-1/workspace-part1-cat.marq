70cbc0aed2ad8e8e3b7fec0021c4f71985ab6e1f
Cat MarquezMontas <catherine.marquezmontas@tlgcohort.com>
2023-02-22 15:20:46 -0800

    Lab 10.1-Television in-class work

---
+++ Lab10.1-Television/src/com/entertainment/DisplayType.java
@@ -0,0 +1,10 @@
+package com.entertainment;
+
+/*
+ * These are the DisplayType objects (Not Strings)
+ * These are the ONLY 5 DisplayType objects possible - a fixed set of named instances
+ * Referenced as DisplayType.LED, DisplayType.LCS, etc.
+ */
+public enum DisplayType {
+    LED, OLED, PLASMA, LCD, CRT
+}
